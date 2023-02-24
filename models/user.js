df38eb88166bb62bdcd1a6d1749aab1d3e16e394
Nidhi <vyasnidhi0601@gmail.com>
2023-02-24 22:03:56 +0530

    initial commit

---
+++ models/user.js
@@ -0,0 +1,55 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      User.hasOne(models.Role, {
+        foreignKey: "user_id",
+        as: "role",
+      });
+    }
+  }
+
+  User.init(
+    {
+      role_id: DataTypes.INTEGER,
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      fullname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+
+  User.beforeSave(async (user, options) => {
+    if (user.password) {
+      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    }
+  });
+  User.prototype.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+  }
+  
+  return User;
+};
